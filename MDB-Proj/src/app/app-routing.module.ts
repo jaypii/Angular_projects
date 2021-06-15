13950598d596092500332018df1cfa7cf38b5fbc
jaypii <jpomv2@icloud.com>
2021-06-15 17:25:33 +0200

    Angular 12 update 210615

--- MDB-Proj/src/app/app-routing.module.ts
+++ MDB-Proj/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent }  from './components/home/home.component';
 import { AboutComponent }  from './components/about/about.component';
-import { NWN2CharsListComponent} from './components/nwn2chars-list/nwn2chars-list.component'
+import { NWN2CharsListComponent} from './components/nwn2chars-list/nwn2chars-list.component';
+import { ContactComponent} from './components/contact/contact.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: 'home', pathMatch: 'full' }, 
   { path: 'home', component: HomeComponent }, 
   { path: 'about', component: AboutComponent },
-  { path: 'nwn2chars', component: NWN2CharsListComponent } 
+  { path: 'nwn2chars', component: NWN2CharsListComponent },
+  { path: 'contact', component: ContactComponent }  
 ];
 
 @NgModule({
