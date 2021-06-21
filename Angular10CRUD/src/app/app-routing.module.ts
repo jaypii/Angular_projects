afba21826bb5be1292827e6a0af047e332494010
jaypii <jpomv2@icloud.com>
2021-06-21 10:06:16 +0200

    Angular 12 update 210621

--- Angular10CRUD/src/app/app-routing.module.ts
+++ Angular10CRUD/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { NWN2CharsListComponent } from './components/nwn2chars-list/nwn2chars-list.component';
 import { NWN2CharDetailsComponent } from './components/nwn2char-details/nwn2char-details.component';
 import { AddNWN2CharComponent } from './components/add-nwn2char/add-nwn2char.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'nwn2chars', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'nwn2chars', component: NWN2CharsListComponent },
   { path: 'nwn2chars/:id', component: NWN2CharDetailsComponent },
   { path: 'addnwn', component: AddNWN2CharComponent }
