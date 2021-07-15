764f54e0e5c029f875b82b870930db6069af2bb6
jaypii <jpomv2@icloud.com>
2021-07-15 17:34:02 +0200

    Angular update 210715

--- Angular10CRUD/src/app/app.component.ts
+++ Angular10CRUD/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { version } from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular10CRUD';
+  public version: string = version;
   todayDate = new Date();
   data: any;
 }
